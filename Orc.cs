7f5207f5da4d367db076a7c919f2af84fb41704c
MallousX <125490986+MallousX@users.noreply.github.com>
2023-03-15 19:59:29 -0500

    now using switch instead of if's

--- Orc.cs
+++ Orc.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Avalonis
 {
-    internal class Orc
+    class Orc
     {
+        public string Name { get; private set; }
+        public int Health { get; set; }
+        public int Damage { get; private set; }
+
+        public Orc(string name, int health, int damage)
+        {
+            Name = name;
+            Health = health;
+            Damage = damage;
+        }
+
+        public void Attack()
+        {
+            // Perform attack logic here
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+        }
     }
 }
